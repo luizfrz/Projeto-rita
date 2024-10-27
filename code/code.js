1de7d6ccda4324a0f8d0ee50a9d91bde072c6991
luiz frança <kazumi200516@gmail.com>
2024-10-27 14:45:31 -0300

    alterações de fontes e otimizado para celular

--- code/code.js
+++ code/code.js
@@ -1,2 +1,7 @@
-var click = document.getElementById("aperta")
+const aperta = document.getElementById("apertado")
+aperta.addEventListener("click",function(
 
+    alert("jesus te ama ")
+) {
+    
+})
